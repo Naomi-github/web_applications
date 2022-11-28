b5e4de30d6128399d7eb589f535267a5d7f34358
Naomi Starr <naomi.starr.mail@gmail.com>
2022-11-28 14:00:09 +0000

    get and post web project

--- hello_web_project/app.rb
+++ hello_web_project/app.rb
@@ -1,3 +1,4 @@
+# file: app.rb
 require 'sinatra/base'
 require 'sinatra/reloader'
 
@@ -6,5 +7,32 @@ class Application < Sinatra::Base
   # without having to restart the server.
   configure :development do
     register Sinatra::Reloader
+  end
+
+  # Declares a route that responds to a request with:
+  #  - a GET method
+  #  - the path /
+  get '/' do
+    # The code here is executed when a request is received,
+    # and need to send a response.
+
+    # We can simply return a string which
+    # will be used as the response content.
+    # Unless specified, the response status code
+    # will be 200 (OK).
+    return 'Some response data'
+  end
+
+  get '/hello' do
+    name = params[:name]
+
+    return "Hello #{name}"
+  end
+
+  post '/submit' do
+    name = params[:name]
+    message = params[:message]
+
+    return "Thanks #{name}, you sent this message: #{message}"
   end
 end
